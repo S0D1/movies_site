bf67b8064cd6f95345f714d89dd3a16019f3a4bf
S0D1 <82319735+S0D1@users.noreply.github.com>
2024-06-14 15:54:16 +0300

    fin vers

---
+++ app/base_functions.py
@@ -0,0 +1,8 @@
+from app.database import Base, engine
+
+
+def create_tables():
+    engine.echo = False
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    engine.echo = False
---
+++ forms.py
@@ -0,0 +1,37 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
+from app.database import Session
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm your password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Зарегистрироваться')
+
+    def validate_username(self, username):
+        db = Session()
+        user = db.query(User).filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Это имя пользователя занято, попробуйте другое.')
+
+    def validate_email(self, email):
+        db = Session()
+        user = db.query(User).filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Этот email уже зарегистрирован.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class ReviewForm(FlaskForm):
+    content = TextAreaField('Ваш отзыв', validators=[DataRequired()])
+    submit = SubmitField('Отправить')
